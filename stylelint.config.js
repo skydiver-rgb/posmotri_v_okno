70d307b71648e8bfaf30ba6405c67fbdb9baafd4
Anton Bogdanov <skydiverivanov@yandex.ru>
2024-10-22 22:17:24 +0300

    на проверку

---
+++ stylelint.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    rules: {
+        "declaration-block-no-duplicate-custom-properties": true,
+        "declaration-block-no-duplicate-properties": true,
+        "font-family-no-duplicate-names": true,
+        "no-duplicate-selectors": true
+    }
+  }
